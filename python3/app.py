b8ce1586f6c4491d5956387a334f970ef068be62
matt.ettler <mattettler@protonmail.com>
2023-08-17 20:09:01 -0600

    format: fix flake8 issues

--- python3/app.py
+++ python3/app.py
@@ -1,16 +1,18 @@
+'''Entry Script. This is the main scrupt which is intended to be run to play
+worlds above'''
 # Inlude our component and system folders into the module path.
 import os
 import sys
-
-app_dir = os.path.dirname( __file__ )
-systems_dir = os.path.join( app_dir, 'systems')
-components_dir = os.path.join( app_dir, 'components')
-sys.path.append( systems_dir )
-sys.path.append( components_dir )
-
-from config import config
-import entitymanager
 import pygame
+from config import config
+
+import entitymanager
+app_dir = os.path.dirname(__file__)
+systems_dir = os.path.join(app_dir, 'systems')
+components_dir = os.path.join(app_dir, 'components')
+sys.path.append(systems_dir)
+sys.path.append(components_dir)
+
 
 screenWidth = config.getint('Graphics', 'ScreenWidth')
 screenHeight = config.getint('Graphics', 'ScreenHeight')
@@ -19,8 +21,8 @@ mapSize = config.getint('Graphics', 'MapSize')
 pygame.init()
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 clock = pygame.time.Clock()
-running = True
-delta_time_milliseconds = 0
+RUNNING = True
+DELTA_TIME_MILLISECONDS = 0
 
 # Import our game-components.
 import boundingbox
@@ -40,7 +42,11 @@ fortressHeight = config.getint('Fortress', 'Height')
 fortressRed = config.getint('Fortress', 'Red')
 fortressGreen = config.getint('Fortress', 'Green')
 fortressBlue = config.getint('Fortress', 'Blue')
-fortress = boundingbox.BoundingBox((mapSize-fortressWidth)/2, (mapSize-fortressHeight)/2, fortressWidth, fortressHeight, 0, fortressRed, fortressGreen, fortressBlue)
+fortress = boundingbox.BoundingBox(
+        (mapSize-fortressWidth)/2, (mapSize-fortressHeight)/2,
+        fortressWidth, fortressHeight,
+        0,
+        fortressRed, fortressGreen, fortressBlue)
 boundingbox.bounding_boxes[entitymanager.create_entity()] = fortress
 
 # Populate characters on the map
@@ -51,22 +57,22 @@ charactersystem.bootstrap_characters(mapSize)
 # MAIN PROGRAM LOOP #
 #####################
 
-while running:
+while RUNNING:
     # Check for program termination
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
-            running = False
+            RUNNING = False
     keys = pygame.key.get_pressed()
     if keys[pygame.K_ESCAPE]:
-        running = False
+        RUNNING = False
 
     # Logical Updates
-    charactersystem.update_characters(fortress, delta_time_milliseconds)
+    charactersystem.update_characters(fortress, DELTA_TIME_MILLISECONDS)
 
     # Graphical Updates
-    screen.fill((10,10,80))
+    screen.fill((10, 10, 80))
     boundingboxsystem.render(screen, screenWidth/mapSize, screenHeight/mapSize)
     pygame.display.flip()
 
-    delta_time_milliseconds = clock.tick(60)
+    DELTA_TIME_MILLISECONDS = clock.tick(60)
 pygame.quit()
