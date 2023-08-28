88cb717a0370931099f1fdf23f74210aa98ca35c
matt.ettler <mattettler@protonmail.com>
2023-08-27 22:20:47 -0600

    feat(boundingboxvelocitysystem): rotate boxes in the direction they are moving.

--- python3/systems/boundingboxvelocitysystem.py
+++ python3/systems/boundingboxvelocitysystem.py
@@ -1,3 +1,4 @@
+import math
 from boundingbox import bounding_boxes
 from velocity import velocities
 
@@ -8,6 +9,10 @@ from velocity import velocities
 def apply_velocities_to_bounding_boxes(lapsed_milliseconds: int):
     for entity in velocities.keys():
         if bounding_boxes.get(entity, None) is not None:
+            bounding_boxes[entity].rotation_degrees = math.degrees(math.atan2(
+                    max(-1, min(velocities[entity].x_delta_per_millisecond, 1)),
+                    max(-1, min(velocities[entity].y_delta_per_millisecond, 1))
+                    ))
             bounding_boxes[entity].x \
                 += velocities[entity].x_delta_per_millisecond \
                 * lapsed_milliseconds
